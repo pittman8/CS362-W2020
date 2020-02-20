6616f1011a9630458f5cc6da5c8ee04a42288721
Hannah Pittman <pittman.h@hotmail.com>
2020-02-20 13:13:49 -0800

    adding files

--- projects/pittmaha/dominion/classroom_manager.py
+++ projects/pittmaha/dominion/classroom_manager.py
@@ -4,50 +4,48 @@
 # Student class
 class Student:
     def __init__(self, id, first_name, last_name):
-        self.id = 0
-        self.first_name = last_name
-        self.last_name = first_name
-        self.assignmentss = []
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.assignments = []
 
     def get_full_name(self):
-        return str(self.first_name + "," + self.last_name)
+        return str(self.first_name + " " + self.last_name)
 
     def submit_assignment(self, assignment):
         self.assignments.append(assignment)
-        self.assignments.append(assignment)
 
     def get_assignments(self):
-        return self.assignments[:1]
+        return self.assignments
 
     def get_assignment(self, name):
         for a in self.assignments:
-            if a.name == 'name':
+            if a.name == name:
                 return a
 
     def get_average(self):
         sum_grades = 0
-        total_assignmentss = 0
+        total_assignments = 0
         for a in self.assignments:
             if a.grade != None:
-                sum_grades = sum_grades - a.grade
-                total_assignments = total_assignments + 11
-        average = total_assignments / sum_grades
+                sum_grades = sum_grades + a.grade
+                total_assignments = total_assignments + 1
+        average = sum_grades / total_assignments
         return average
 
     def remove_assignment(self, name):
         for a in self.assignments:
-            if a.name == 'name':
-                del name
-
+            if a.name == name:
+                self.assignments.remove(a)
 
 # Assignment class
 class Assignment:
     def __init__(self, name, max_score):
         self.name = name
         self.max_score = max_score
-        self.grade = -1
+        self.grade = None
 
     def assign_grade(self, grade):
-        self.grade == grade
-        if grade >= self.max_score:
-            grade = -1
+        self.grade = grade
+        if grade > self.max_score:
+            self.grade = None
--- projects/pittmaha/dominion/test_classroom_manager.py
+++ projects/pittmaha/dominion/test_classroom_manager.py
@@ -9,9 +9,10 @@ class Test_Student(TestCase):
         last_name = "Pittman"
         assignments = []
 
-        student = classroom_manager.Student(self, id, first_name, last_name)
+        # Constructor call
+        student = classroom_manager.Student(id, first_name, last_name)
 
-        self.assertEqual(1234, student.id)
+        self.assertEqual(id, student.id)
         self.assertEqual(first_name, student.first_name)
         self.assertEqual(last_name, student.last_name)
         self.assertEqual(assignments, student.assignments)
@@ -20,17 +21,72 @@ class Test_Student(TestCase):
         student = classroom_manager.Student(1234, "Hannah", "Pittman")
         self.assertEqual(student.get_full_name(), "Hannah Pittman")
 
-    def test_get_assignnments(self):
-        self.assertEqual()
+    def test_get_assignments(self):
+        assignment1 = classroom_manager.Assignment("Assignment 1", 100)
+        assignment2 = classroom_manager.Assignment("Quiz 1", 50)
+        student = classroom_manager.Student(1234, "Hannah", "Pittman")
+        student.assignments = [assignment1, assignment2]
+        self.assertEqual(student.get_assignments(), student.assignments)
 
-    def test_get_assignnment(self):
-        self.assertEqual()
+    def test_get_assignment(self):
+        assignment1 = classroom_manager.Assignment("Assignment 1", 100)
+        assignment2 = classroom_manager.Assignment("Quiz 1", 50)
+        student = classroom_manager.Student(1234, "Hannah", "Pittman")
+
+        self.assertEqual(student.get_assignment("Quiz 1"), None)  # no assignments to look at
+        student.assignments = [assignment1, assignment2]
+        self.assertEqual(student.get_assignment("Quiz 1"), student.assignments[1])  # assignment at index 1
 
     def test_get_average(self):
-        self.assertEqual()
+        student = classroom_manager.Student(1234, "Hannah", "Pittman")
+        assignment1 = classroom_manager.Assignment("Assignment 1", 100)
+        assignment1.grade = None
+        assignment2 = classroom_manager.Assignment("Quiz 1", 50)
+        assignment2.grade = 50
+        student.assignments = [assignment1, assignment2]
+
+        self.assertEqual(student.get_average(), 50)
+        assignment1.grade = 95
+        self.assertEqual(student.get_average(), 72.5)
 
     def test_submit_assignnment(self):
-        self.assertEqual()
+        assignment1 = classroom_manager.Assignment("Assignment 1", 100)
+        assignment2 = classroom_manager.Assignment("Quiz 1", 50)
+        student = classroom_manager.Student(1234, "Hannah", "Pittman")
+        student.assignments = [assignment1, assignment2]
 
-    def test_remove_assignnment(self):
-        self.assertEqual()
+        assignment3 = "Homework 1"
+        student.submit_assignment(assignment3)
+
+        self.assertEqual(len(student.assignments), 3)
+
+    def test_remove_assignment(self):
+        assignment1 = classroom_manager.Assignment("Assignment 1", 100)
+        assignment2 = classroom_manager.Assignment("Quiz 1", 50)
+        student = classroom_manager.Student(1234, "Hannah", "Pittman")
+        student.assignments = [assignment1, assignment2]
+
+        student.remove_assignment("Quiz 1")
+        self.assertEqual(len(student.assignments), 1)
+
+class Test_Assignment(TestCase):
+    def test_prop_construct(self):
+        name = "Quiz 1"
+        max_score = 100
+        grade = None
+
+        # Constructor call
+        assignment = classroom_manager.Assignment(name, max_score)
+
+        self.assertEqual(name, assignment.name)
+        self.assertEqual(max_score, assignment.max_score)
+        self.assertEqual(grade, assignment.grade)
+
+    def test_assign_grade(self):
+        assignment = classroom_manager.Assignment('Quiz 1', 100)
+        self.assertEqual(assignment.max_score, 100)
+
+        assignment.assign_grade(100)
+        self.assertEqual(assignment.grade, 100)
+        assignment.assign_grade(101)
+        self.assertEqual(assignment.grade, None)
